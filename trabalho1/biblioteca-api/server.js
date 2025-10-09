40400b13626ae11f6c709f07b3b4fea23d23880a
Pedro-henriquebg86 <pedrohenriquebg86@gmail.com>
2025-10-09 12:53:56 -0300

    Add files via upload

--- trabalho1/biblioteca-api/server.js
+++ trabalho1/biblioteca-api/server.js
@@ -1,14 +1,22 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 
 const livrosRoutes = require("./routes/livros");
 const usuariosRoutes = require("./routes/usuarios");
+const autoresRoutes = require("./routes/autores");
+const editorasRoutes = require("./routes/editoras");       // <<-- nova
+const emprestimosRoutes = require("./routes/emprestimos"); // <<-- nova
 
 const app = express();
-app.use(bodyParser.json());
 
+// Usando JSON nativo do Express
+app.use(express.json());
+
+// Rotas
 app.use("/livros", livrosRoutes);
 app.use("/usuarios", usuariosRoutes);
+app.use("/autores", autoresRoutes);
+app.use("/editoras", editorasRoutes);       
+app.use("/emprestimos", emprestimosRoutes); 
 
 const PORT = 3000;
 app.listen(PORT, () => {
