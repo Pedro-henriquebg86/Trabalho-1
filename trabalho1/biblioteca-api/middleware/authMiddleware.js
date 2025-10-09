3497617aaf21c8d6168b86635703b81b05dd265a
Pedro-henriquebg86 <pedrohenriquebg86@gmail.com>
2025-10-09 12:50:46 -0300

    Add files via upload

--- trabalho1/biblioteca-api/middleware/authMiddleware.js
+++ trabalho1/biblioteca-api/middleware/authMiddleware.js
@@ -1,12 +1,16 @@
 const jwt = require("jsonwebtoken");
 
 function autenticarToken(req, res, next) {
-  const token = req.headers["authorization"];
+  const authHeader = req.headers["authorization"];
+  if (!authHeader) return res.status(401).json({ msg: "Token não fornecido" });
+
+  // Extrai apenas o token depois de "Bearer "
+  const token = authHeader.split(" ")[1];
   if (!token) return res.status(401).json({ msg: "Token não fornecido" });
 
   jwt.verify(token, "segredo123", (err, usuario) => {
     if (err) return res.status(403).json({ msg: "Token inválido" });
-    req.usuario = usuario;
+    req.usuario = usuario; // id e role do usuário
     next();
   });
 }
--- trabalho1/biblioteca-api/middleware/roleMiddleware.js
+++ trabalho1/biblioteca-api/middleware/roleMiddleware.js
@@ -1,11 +1,10 @@
 function autorizarRole(role) {
-    return (req, res, next) => {
-      if (req.usuario.role !== role) {
-        return res.status(403).json({ msg: "Acesso negado" });
-      }
-      next();
-    };
-  }
-  
-  module.exports = autorizarRole;
-  
+  return (req, res, next) => {
+    if (req.usuario.role !== role) {
+      return res.status(403).json({ msg: "Acesso negado" });
+    }
+    next();
+  };
+}
+
+module.exports = autorizarRole;
