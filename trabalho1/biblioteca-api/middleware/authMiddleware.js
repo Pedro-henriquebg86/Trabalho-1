6309d6847003123c2b1d6c4558ec4d394a281b38
Pedro-henriquebg86 <pedrohenriquebg86@gmail.com>
2025-10-10 17:47:03 -0300

    Update authMiddleware.js

--- trabalho1/biblioteca-api/middleware/authMiddleware.js
+++ trabalho1/biblioteca-api/middleware/authMiddleware.js
@@ -4,13 +4,13 @@ function autenticarToken(req, res, next) {
   const authHeader = req.headers["authorization"];
   if (!authHeader) return res.status(401).json({ msg: "Token não fornecido" });
 
-  // Extrai apenas o token depois de "Bearer "
+  // Extrai o token após o prefixo Bearer
   const token = authHeader.split(" ")[1];
-  if (!token) return res.status(401).json({ msg: "Token não fornecido" });
+  if (!token) return res.status(401).json({ msg: "Token ausente" });
 
   jwt.verify(token, "segredo123", (err, usuario) => {
     if (err) return res.status(403).json({ msg: "Token inválido" });
-    req.usuario = usuario; // id e role do usuário
+    req.usuario = usuario;
     next();
   });
 }
